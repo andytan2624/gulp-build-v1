d8d5df26695656c4acf69756db436a2e1b864daf
andytan2624 <andy.tan2624@gmail.com>
2016-12-31 13:31:11 +1100

    Update gulpfile tasks to include watch and serve task, and also fixing scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 "use strict";
 
 var gulp = require('gulp'),
-    useref = require('gulp-useref'),
+    concat = require('gulp-concat'),
     iff = require('gulp-if'),
     uglify = require('gulp-uglify'),
     maps = require('gulp-sourcemaps'),
     sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
     del = require('del'),
     imagemin = require('gulp-imagemin'),
-    eslint = require('gulp-eslint')
+    eslint = require('gulp-eslint'),
+    server = require('gulp-server-livereload')
     ;
 
 var options = {
@@ -16,19 +18,20 @@ var options = {
     dist: 'dist'
 };
 
-gulp.task('scripts', function() {
-    return gulp.src(options.src + '/index.html')
-        .pipe(useref())
+gulp.task('scripts', function () {
+    return gulp.src(['js/global.js', 'js/circle/circle.js', 'js/circle/autogrow.js'])
+        .pipe(maps.init())
+        .pipe(concat('all.js'))
         .pipe(iff('*.js', eslint({configFile: options.src + '/js/eslint.json'})))
         .pipe(iff('*.js', eslint.format()))
         .pipe(iff('*.js', eslint.failAfterError()))
-        .pipe(maps.init())
-        .pipe(iff('*.js', uglify()))
+        .pipe(uglify())
+        .pipe(rename('all.min.js'))
         .pipe(maps.write('./'))
-        .pipe(gulp.dest(options.dist));
+        .pipe(gulp.dest(options.dist + '/js'));
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     return gulp.src(options.src + '/sass/global.scss')
         .pipe(maps.init())
         .pipe(sass())
@@ -36,24 +39,44 @@ gulp.task('styles', function() {
         .pipe(gulp.dest(options.dist + '/css'))
 });
 
-gulp.task('images', function(){
+gulp.task('watch', function () {
+    gulp.watch(['sass/**/*.scss', 'sass/**/*.sass'], ['styles']);
+    gulp.watch(['js/global.js', 'js/circle/circle.js', 'js/circle/autogrow.js'], ['scripts']);
+});
+
+gulp.task('images', function () {
     gulp.src(options.src + '/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest(options.dist + '/content'))
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     del(['dist'])
 });
 
-gulp.task('build', ['clean'], function() {
+gulp.task('serve', function () {
+    gulp.start('watch');
+    gulp.src('dist')
+        .pipe(server({
+            defaultFile: 'index.html',
+            livereload: true,
+            directoryListing: false,
+            open: true,
+            log: 'debug',
+            clientConsole: true
+        }));
+});
+
+gulp.task('build', ['clean'], function () {
     gulp.start('scripts');
     gulp.start('styles');
     gulp.start('images');
     gulp.src(options.src + '/icons/*')
-        .pipe(gulp.dest(options.dist + '/icons' ))
+        .pipe(gulp.dest(options.dist + '/icons'));
+    gulp.src(options.src + '/index.html')
+        .pipe(gulp.dest(options.dist));
 });
 
-gulp.task('default', ['clean'], function() {
+gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
